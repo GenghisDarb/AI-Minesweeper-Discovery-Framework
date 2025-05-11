7b12b4d673f9f62c4a7c6b642b546d041019e40b
Bradley Peter <bradleypeter1984@gmail.com>
2025-05-11 06:12:23 -0700

    Create board_builder.py

---
+++ src/ai_minesweeper/board_builder.py
@@ -0,0 +1,8 @@
+class BoardBuilder:
+    """Ingests domain relations & builds cell network."""
+    def __init__(self, relations):
+        self.relations = relations
+
+    def build(self):
+        # TODO: Implement cell network construction
+        pass
