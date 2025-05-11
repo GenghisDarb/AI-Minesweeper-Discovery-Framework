37eb6b410e5aa40a5d8ff271c6c5d17cfda2f66e
Bradley Peter <bradleypeter1984@gmail.com>
2025-05-11 06:13:42 -0700

    Create risk_assessor.py

---
+++ src/ai_minesweeper/risk_assessor.py
@@ -0,0 +1,8 @@
+class RiskAssessor:
+    """Scores unknown cells for next probe."""
+    def __init__(self, board):
+        self.board = board
+
+    def score_cells(self):
+        # TODO: Implement risk scoring
+        pass
