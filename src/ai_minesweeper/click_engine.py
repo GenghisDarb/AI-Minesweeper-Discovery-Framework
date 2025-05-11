e6515515b064c2728a83579c8233540265f2977e
Bradley Peter <bradleypeter1984@gmail.com>
2025-05-11 06:13:00 -0700

    Create click_engine.py

---
+++ src/ai_minesweeper/click_engine.py
@@ -0,0 +1,8 @@
+class ClickEngine:
+    """Propagates constraints, reveals safe cells."""
+    def __init__(self, board):
+        self.board = board
+
+    def reveal_safe_cells(self):
+        # TODO: Implement constraint propagation
+        pass
