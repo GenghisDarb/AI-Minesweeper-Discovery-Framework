9cdfaef5591662f4732f4e1c3674d264e3a45834
Bradley Peter <bradleypeter1984@gmail.com>
2025-05-14 04:05:38 -0700

    Create test_click_engine.py

---
+++ tests/test_click_engine.py
@@ -0,0 +1,13 @@
+import unittest
+from ai_minesweeper.board_builder import BoardBuilder
+from ai_minesweeper.click_engine import ClickEngine
+
+class TestClickEngine(unittest.TestCase):
+    def test_next_click_within_bounds(self):
+        board = BoardBuilder.from_csv('examples/boards/mini.csv')
+        r, c = ClickEngine.next_click(board)
+        self.assertTrue(0 <= r < board.n_rows)
+        self.assertTrue(0 <= c < board.n_cols)
+
+if __name__ == '__main__':
+    unittest.main()
