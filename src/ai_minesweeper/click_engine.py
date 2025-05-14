6094f79708ef6025cca182cb2c5c53cf52b07e93
Bradley Peter <bradleypeter1984@gmail.com>
2025-05-14 03:59:24 -0700

    Update click_engine.py

--- src/ai_minesweeper/click_engine.py
+++ src/ai_minesweeper/click_engine.py
@@ -6,3 +6,16 @@ class ClickEngine:
     def reveal_safe_cells(self):
         # TODO: Implement constraint propagation
         pass
+    def next_click(self):
+        risk_assessor = RiskAssessor(self._board)
+        risk_map = risk_assessor.estimate()
+
+        # Filter out revealed cells and get the cell with the lowest probability
+        min_prob = float('inf')
+        best_cell = None
+        for (r, c), prob in risk_map.items():
+            if not self._board.grid[r][c].is_revealed and prob < min_prob:
+                min_prob = prob
+                best_cell = (r, c)
+
+        return best_cell
