e5e69c4cbbdde407342e47f4a9cb70d5cb1457f6
Bradley Peter <bradleypeter1984@gmail.com>
2025-05-14 04:10:53 -0700

    Update solver.pyFix linting error in solver.py

--- src/ai_minesweeper/solver.py
+++ src/ai_minesweeper/solver.py
@@ -1,6 +1,8 @@
 class ConstraintSolver:
         def solve(self, board):
-        click_engine = ClickEngine(board)
+        clifrom .click_engine import ClickEngine
+
+ck_engine = ClickEngine(board)
         for _ in range(10):
             r, c = click_engine.next_click()
             board.reveal(r, c)
