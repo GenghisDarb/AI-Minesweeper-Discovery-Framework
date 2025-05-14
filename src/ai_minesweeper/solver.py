bd5e9e00da517412e0f1252e2eb626bdb48cf41c
Bradley Peter <bradleypeter1984@gmail.com>
2025-05-14 04:00:36 -0700

    Update solver.py

--- src/ai_minesweeper/solver.py
+++ src/ai_minesweeper/solver.py
@@ -1,4 +1,7 @@
 class ConstraintSolver:
-    def solve(self, board):
-        # Placeholder implementation
-        pass
+        def solve(self, board):
+        click_engine = ClickEngine(board)
+        for _ in range(10):
+            r, c = click_engine.next_click()
+            board.reveal(r, c)
+
