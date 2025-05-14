aafc0d48fef1e6c4345cb0afe7b78f38573ec000
Bradley Peter <bradleypeter1984@gmail.com>
2025-05-14 03:58:19 -0700

    Update risk_assessor.py

--- src/ai_minesweeper/risk_assessor.py
+++ src/ai_minesweeper/risk_assessor.py
@@ -6,3 +6,16 @@ class RiskAssessor:
     def score_cells(self):
         # TODO: Implement risk scoring
         pass
+
+    def estimate(self) -> dict:
+        risk_map = {}
+        for r in range(self._board.n_rows):
+            for c in range(self._board.n_cols):
+                cell = self._board.grid[r][c]
+                if cell.is_flagged:
+                    risk_map[(r, c)] = 1.0
+                elif cell.is_revealed:
+                    risk_map[(r, c)] = 0
+                else:
+                    risk_map[(r, c)] = 0.15
+        return risk_map
