cc5c6a201204e1e5ef7beef3a350056020528d62
Bradley Peter <bradleypeter1984@gmail.com>
2025-07-13 12:20:35 +0000

    Fix F821 errors in BetaConfidence by defining class and method

--- src/ai_minesweeper/beta_confidence.py
+++ src/ai_minesweeper/beta_confidence.py
@@ -1,4 +1,10 @@
-if revealed_is_false:
-    self.alpha += 0.5
-else:
-    self.beta += 0.5
+class BetaConfidence:
+    def __init__(self):
+        self.alpha = 0.0
+        self.beta = 0.0
+
+    def update(self, revealed_is_false: bool):
+        if revealed_is_false:
+            self.alpha += 0.5
+        else:
+            self.beta += 0.5
